26aec66c102821ebdf24fac78d33c185359ccb4b
Sung Yong, Park <40999564+psyoongsc@users.noreply.github.com>
2021-10-22 21:42:14 +0900

    errorlog add

--- Server.js
+++ Server.js
@@ -38,6 +38,53 @@ app.post('/recentSensor', function (request, response) {
     });
 });
 
-app.listen(3000, function() {
-    console.log('Server Running at http://127.0.0.1:3000');
+app.post('/errorlog', function (request, response) {
+    var errors = [];
+
+    pool.getConnection(function(err, conn) {
+        if(!err) {
+            conn.query('SELECT datetime FROM diagResult ORDER BY seq_num DESC', function(error, results, fileds) {
+                if(error) {
+                    console.log(error);
+                }
+        
+                results.forEach(function(row) {
+                    errors.push(row.datetime);
+                });
+        
+                response.send(errors);
+            });
+        }
+
+        conn.release();
+        console.log("error log response SUCCESS!")
+    });
+});
+
+app.post('/sensor', function (request, response) {
+    var yaxis = [];
+
+    pool.getConnection(function(err, conn) {
+        if(!err) {
+            conn.query('SELECT * FROM sensorData WHERE ch = ' + `${request.body.ch}` + ' and timestamp >= "' + `${request.body.start}` + '" ORDER BY seq_num ASC limit 500', 
+            function(error, results, fileds) {
+                if(error) {
+                    console.log(error);
+                }
+        
+                results.forEach(function(row) {
+                    yaxis.push(row.value);
+                });
+        
+                response.send(yaxis);
+            });
+        }
+
+        conn.release();
+        console.log("channel " + `${request.body.ch}` + " recentSensor response SUCCESS!")
+    });
+});
+
+app.listen(80, '0.0.0.0', function() {
+    console.log('Server Running at http://spin.developerpsy.com');
 });
